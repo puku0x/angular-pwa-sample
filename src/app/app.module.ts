4b31787db27c6c1184cc9cb61da515e07a26b3eb
puku0x <puku0x@gmail.com>
2017-12-17 15:18:36 +0900

    pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { MatToolbarModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production }),
     MatToolbarModule,
-    AppRoutingModule
   ],
   providers: [
     { provide: LocationStrategy, useClass: HashLocationStrategy }
---
+++ src/app/app.server.module.ts
@@ -0,0 +1,22 @@
+import { ServerModule } from '@angular/platform-server';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MatProgressBarModule } from '@angular/material/';
+
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
+import { AppShellComponent } from './app-shell/app-shell.component';
+
+const routes: Routes = [ { path: 'shell', component: AppShellComponent }];
+
+@NgModule({
+  imports: [
+    AppModule,
+    ServerModule,
+    RouterModule.forRoot(routes),
+    MatProgressBarModule,
+  ],
+  bootstrap: [AppComponent],
+  declarations: [AppShellComponent],
+})
+export class AppServerModule {}
