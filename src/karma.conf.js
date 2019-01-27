32a1a0b2dbc01c89cdb403c8cfa20a13dcdc09d2
puku0x <puku0x@gmail.com>
2019-01-27 23:18:27 +0900

    add ChromeHeadlessCI

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -12,6 +12,12 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
